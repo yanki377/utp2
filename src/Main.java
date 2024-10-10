8817ea949c3517b1aac262db4683cf4cdfb642e9
yanki377 <117318512+yanki377@users.noreply.github.com>
2024-10-10 16:19:43 +0200

    added ur current task

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
-        Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
-        Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
-    }
-}
+ublic class Main {
+ public static void main(String[] args) {
+ Adder adder = new Adder();
+ System.out.println(adder.add(1, 2));
+ Subtractor subtractor = new Subtractor();
+ System.out.println(subtractor.subtract(6, 3));
+ }

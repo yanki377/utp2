86439dfc643cd6c8d7b46f43cbb522a3193c24a4
mustafa yanki <yankiguzel7@gmail.com>
2024-10-10 16:26:01 +0200

    yes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+// TODO: we need to add the missing classes!
+// OK, I will add `Adder` and s##### will add `Subtractor`.
+
 public class Main {
  public static void main(String[] args) {
- Adder adder = new Adder();
- System.out.println(adder.add(1, 2));
- Subtractor subtractor = new Subtractor();
- System.out.println(subtractor.subtract(6, 3));
+  Adder adder = new Adder();
+  System.out.println(adder.add(1, 2));
+  Subtractor subtractor = new Subtractor();
+  System.out.println(subtractor.subtract(6, 3));
  }
 }
